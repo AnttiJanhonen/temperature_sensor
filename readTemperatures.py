33eb0413fbfe97c551e258eb90bfa5d06e0a01c4
Antti <antti.janhonen@pp.inet.fi>
2017-11-18 19:48:04 +0200

    Refactoring

--- readTemperatures.py
+++ readTemperatures.py
@@ -14,43 +14,57 @@ influxdb_db = "?db=Lämpötilat"
 region = "mokki"
 server = "xorppo"
 influxdb_full_url = "http://{0}{1}{2}".format(influxdb_address, influxdb_api, influxdb_db)
-
-
-def readTemperatures():
-    return "ph"
-
-
-avgtemperatures = []
+# avgtemperatures = []
 # Find out which sensors exist assuming all devices connected to the pi are temperature sensors
 sensor_paths = glob("/sys/bus/w1/devices/*/")
 regex = re.compile(r'(([^\']+)w1_bus_master1([^\']+))')
 # Remove w1_bus_master from the array as it is not a sensor.
 sensor_paths = [x for x in sensor_paths if not regex.match(x)]
-# Loop through the sensors and read them and write their value to avgtemperatures array.
-for sensor in range(len(sensor_paths)):
-    temperatures = []
-    for polltime in range(0, 3):
-        text = ''
-        while text.split("\n")[0].find("YES") == -1:
-            tfile = open(sensor_paths[sensor] + "w1_slave")
-            text = tfile.read()
-            tfile.close()
-            time.sleep(1)
 
-        secondline = text.split("\n")[1]
-        temperaturedata = secondline.split(" ")[9]
-        temperature = round(float(temperaturedata[2:]), 3)
-        temperatures.append(temperature / 1000)
 
-    realtemp = round(sum(temperatures) / float(len(temperatures)), 3)
-    print realtemp
-    payload = 'lampotila,host={0},region={1},sensor={2} value={3}'.format(server, region, sensor_paths[sensor], realtemp)
+def read_temperatures():
+    # Loop through the sensors and read them and write their value to avgtemperatures array.
+    avgtemperatures = []
+    for sensor in range(len(sensor_paths)):
+        temperatures = []
+        for polltime in range(0, 3):
+            text = ''
+            while text.split("\n")[0].find("YES") == -1:
+                tfile = open(sensor_paths[sensor] + "w1_slave")
+                text = tfile.read()
+                tfile.close()
+                time.sleep(1)
+
+            secondline = text.split("\n")[1]
+            temperaturedata = secondline.split(" ")[9]
+            temperature = round(float(temperaturedata[2:]), 3)
+            temperatures.append(temperature / 1000)
+
+        realtemp = round(sum(temperatures) / float(len(temperatures)), 3)
+        print post_temp(sensor, realtemp)
+        avgtemperatures.append(round(sum(temperatures) / float(len(temperatures)), 3))
+    write_temp_to_file(avgtemperatures)
+    # return "Complete"
+
+
+def post_temp(sensor, temp):
+    payload = 'lampotila,host={0},region={1},sensor={2} value={3}'.format(server, region, sensor_paths[sensor], temp)
     response = requests.post(influxdb_full_url, data=payload)
-    print response
-    avgtemperatures.append(round(sum(temperatures) / float(len(temperatures)), 3))
-print (avgtemperatures)
-lampo = str(avgtemperatures)
-file = open(filePath, "w")
-file.write("Temperature:")
-file.write(lampo)
-file.close()
+    return response
+
+
+def main():
+    print read_temperatures()
+
+
+def write_temp_to_file(avgtemperatures):
+    print (avgtemperatures)
+    lampo = str(avgtemperatures)
+    file = open(filePath, "w")
+    file.write("Temperature:")
+    file.write(lampo)
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
